626a53abf96ab4a1f22befc88e987835eab9b5f1
Jason Harrison <jasonharrisonvancouver@gmail.com>
2019-01-29 14:20:27 -0800

    casting done

--- W4D2_Playground.playground/Pages/Type Casting.xcplaygroundpage/Contents.swift
+++ W4D2_Playground.playground/Pages/Type Casting.xcplaygroundpage/Contents.swift
@@ -29,6 +29,10 @@ let myView: UIView = UIButton()
  Try using the `is` operator on `myView`. Take a look at the evaluation on the right to see what the results are. ie: `myView is UIView`. Try checking if `myView` is any of the following views: `UIButton`, `UITableView`, `UIImageView`
  */
 
+print(myView is UIButton)
+print(myView is UIView)
+print(myView is UITableView)
+
 
 /*:
  - Experiment:
@@ -43,7 +47,12 @@ let myView: UIView = UIButton()
  Here we downcast our `myView` variable to an actual UIButton using `as?`. Try changing the type of class you downcast to and check what the behaviour is. For example, what happens when you try to downcast `myView` to a `UITableView`? What happens if you use `as!`?
  */
 
-let myCastedButtonView = myView as? UIButton
+//let myCastedButtonView = myView as? UIButton
+//
+//let v = myView as? UITableView
+//print(v)
+//let w = myView as! UITableView
+
 
 
 /*:
@@ -51,23 +60,57 @@ let myCastedButtonView = myView as? UIButton
  Now that we downcast our `myView` to an actual UIButton, use conditional unwrapping to ensure it is not nil, then try calling the `setTitle` method to ensure it can be called.
  */
 
+//print(myCastedButtonView?.setTitle("hi"
+//    , for: UIControl.State.normal)
+//
+//
+
+
+
 
 /*:
  - Callout(Challenge):
  Every `UIView` has a property called `subviews` that is an array of all of it's subviews. `view.subviews`. Imagine we had a UIView containing many different types of subviews.
  */
 
-let view = UIView()
-view.addSubview(UIButton())
-view.addSubview(UIButton())
-view.addSubview(UILabel())
-view.addSubview(UITextField())
-view.addSubview(UILabel())
-view.addSubview(UIButton())
+
 /*:
  And we need to find all buttons. Write a function to search for `UIButtons` in this view's `subviews`, and return an array will all the buttons.
  */
 
+
+func getAllSubviews(masterView : UIView) -> [UIButton]?{
+    print("testing")
+    guard 0 <= masterView.subviews.count else{
+        print("invalid count of subviews")
+        return nil
+    }
+    
+    var buttons: [UIButton] = [UIButton]()
+    
+    for subview in masterView.subviews {
+        print("checking subview")
+        if subview is UIButton{
+            print("adding button")
+            buttons.append(subview as! UIButton)
+        }
+    }
+    if buttons.count > 0{
+        return buttons
+    }else{
+        return nil
+    }
+}
+
+let view2 = UIView()
+view2.addSubview(UIButton())
+view2.addSubview(UIButton())
+view2.addSubview(UILabel())
+view2.addSubview(UITextField())
+view2.addSubview(UILabel())
+view2.addSubview(UIButton())
+
+print("num button is \(String(describing: getAllSubviews(masterView: view2)?.count))")
 
 /*:
  - Callout(Challenge - Part 1):
@@ -82,16 +125,27 @@ class MediaItem {
 }
 
 class Movie: MediaItem {
+    var director: String
+    init(name: String, director: String) {
+        self.director = director
+        super.init(name: name)
+    }
 }
 
 class Song: MediaItem {
+    var artist: String
+    init(name: String, artist: String){
+        self.artist = artist
+        super.init(name: name)
+    }
 }
+
 
 /*:
  - Callout(Challenge - Part 2):
  Now take the array below of MediaItems and create a for loop on the array that prints out what type of media item it is and print out their properties.
  */
-/*
+
 let library = [
   Movie(name: "Casablanca", director: "Michael Curtiz"),
   Song(name: "Blue Suede Shoes", artist: "Elvis Presley"),
@@ -99,6 +153,16 @@ let library = [
   Song(name: "The One And Only", artist: "Chesney Hawkes"),
   Song(name: "Never Gonna Give You Up", artist: "Rick Astley")
 ]
-*/
+
+for item: MediaItem in library {
+    if item is Movie{
+        let movie: Movie = item as! Movie
+        print("movie named \(movie.name) and directed by \(movie.director)")
+    }else if item is Song{
+        let song: Song = item as! Song
+        print("song named \(song.name) and artist is \(song.artist)")
+    }
+}
+
 
 //: [Next](@next)
